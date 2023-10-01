b34c3e9b0c46ed65476417ed9e17cb135fbd137a
Kauan Soares <gbskauan@gmail.com>
2023-09-30 23:56:38 -0300

    mais um

--- CodTrab1.sql
+++ CodTrab1.sql
@@ -7,6 +7,24 @@ CREATE TYPE estado_recursos_enum AS ENUM ('em uso', 'em manutencao', 'nao dispon
 CREATE TYPE funcao_empregado_enum AS ENUM ('diretor', 'seguranca', 'operador de guindaste', 'operador de carga', 'estivador', 'conferente de carga', 'controlador de trafego', 'pratico', 'oficial de alfandega', 'agente de atendimento', 'RH', 'manutencao', 'outros');
 CREATE TYPE tipo_movimentacao_enum AS ENUM ('carga', 'descarga', 'manutencao', 'estocagem', 'abastecimento de combustivel', 'transferencia de carga', 'outro');
 
+CREATE OR REPLACE FUNCTION adicionar_berco_id()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Verifique se o estado do berço é "ocupado"
+    IF NEW.estado_berco = 'ocupado'::estado_berco_enum THEN
+        -- Insira uma nova B/L com o estado "emb_liberada" definido como TRUE e data/hora atual
+        INSERT INTO bill_of_landing (emb_liberada, quando, berco_id)
+        VALUES (TRUE, CURRENT_TIMESTAMP, NEW.id_berco);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER adicionar_berco_trigger
+AFTER INSERT ON berco_de_atracacao
+FOR EACH ROW
+EXECUTE FUNCTION adicionar_berco_id();
+
 CREATE TABLE empregado (
 	id_empregado SERIAL PRIMARY KEY,
 	nome CHAR(255),
@@ -33,7 +51,9 @@ CREATE TABLE recursos_portuarios (
 	id_recursos SERIAL PRIMARY KEY,
 	nome CHAR(255),
 	tipo tipo_recursos_portuarios_enum,
-	estado estado_recursos_enum
+	estado estado_recursos_enum,
+	id_movimentacao INTEGER,
+	FOREIGN KEY (id_movimentacao) REFERENCES movimentacao
 )
 
 CREATE TABLE empresa (
@@ -42,3 +62,39 @@ CREATE TABLE empresa (
 	cnpj CHAR(14),
 	endereco CHAR(100)
 )
+
+CREATE TABLE movimentacao (
+	id_movimentacao SERIAL PRIMARY KEY,
+	cod_equipe INTEGER,
+	tipo tipo_movimentacao_enum,
+	data_movimentacao DATE
+)
+
+CREATE TABLE alocado_movimentacao_empregado (
+	id_movimentacao INTEGER,
+	id_empregado INTEGER,
+	PRIMARY KEY (id_movimentacao, id_empregado),
+	FOREIGN KEY (id_movimentacao) REFERENCES movimentacao,
+	FOREIGN KEY (id_empregado) REFERENCES empregado
+)
+
+CREATE TABLE berco_de_atracacao (
+	id_berco SERIAL PRIMARY KEY,
+	estado_berco estado_berco_enum
+)
+
+CREATE TABLE bill_of_landing (
+	id_bill SERIAL PRIMARY KEY,
+	emb_liberada BOOL,
+	quando TIMESTAMP,
+	berco_id INTEGER,
+	FOREIGN KEY (berco_id) REFERENCES berco_de_atracacao(id_berco)
+)
+
+DELETE
+FROM berco_de_atracacao B
+WHERE id_berco < 5
+
+SELECT * FROM bill_of_landing
+
+
